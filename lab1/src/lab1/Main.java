c362680af2565074d2ec298c387ff22991073a35
rlyshw <rlyshw@gmail.com>
2015-02-18 15:33:21 -0500

    init commit

---
+++ lab1/src/lab1/Main.java
@@ -0,0 +1,19 @@
+package lab1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	//@SuppressWarnings("resource")
+	public static void main(String args[]) throws IOException {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		String yearsToWork = in.nextLine();
+		String annualReturn = in.nextLine();
+		String yearsRetired = in.nextLine();
+		String requiredIncome = in.nextLine();
+		String monthlySSI = in.nextLine();
+	}
+
+}
