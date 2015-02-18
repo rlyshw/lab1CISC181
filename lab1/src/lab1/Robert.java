e141e14396c26ac26285d78bd4d6b367868ebb4a
Robert Weber <robertweber95@gmail.com>
2015-02-18 16:19:27 -0500

    Added Robert Class (method to calculate pv)

---
+++ lab1/src/lab1/Robert.java
@@ -0,0 +1,14 @@
+package lab1;
+
+public class Robert {
+	
+	 double calculatePV (double retiredReturn, int yearsRetired, 
+			double monthlySSI, double requiredIncome) {
+		
+		double pv = 0.0;
+		
+		return pv;
+	
+	}
+
+}
