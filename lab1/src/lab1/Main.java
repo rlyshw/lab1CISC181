b2bd5c52ee7eecd0ac911af4ec854e7d42b444c3
tkananen <tkananen@hotmail.com>
2015-02-18 16:05:17 -0500

    Added retiredReturn do while loop

--- lab1/src/lab1/Main.java
+++ lab1/src/lab1/Main.java
@@ -10,13 +10,21 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 		String yearsToWork = in.nextLine();
-		double annualReturn; 		
+		double workingReturn; 		
 		do {
-			System.out.println("Please enter your expected annual return as an integer: ");
-			annualReturn = in.nextDouble();
-			} while(annualReturn < 0 || annualReturn > 20);
-		annualReturn = (annualReturn/100);
-		String yearsRetired = in.nextLine();
+			System.out.println("Please enter your expected working return as an integer between 0 and 20: \n"
+					+ "e.g. 7 ");
+			workingReturn = in.nextDouble();
+			} while(workingReturn < 0 || workingReturn > 20);
+		workingReturn /= 100;
+		double retiredReturn;
+		do {
+			System.out.println("Please enter your expected retired return as an integer between 0 and 3: \n"
+					+ "e.g. 2");
+			retiredReturn = in.nextDouble();
+		} while(retiredReturn< 0 || retiredReturn > 3);
+		retiredReturn /= 100;
+		}
 		String requiredIncome = in.nextLine();
 		String monthlySSI = in.nextLine();
 	}
