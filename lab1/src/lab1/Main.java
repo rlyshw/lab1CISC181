9f624abceb6b5e2f017f9221c12e4f2f77714fe3
tkananen <tkananen@hotmail.com>
2015-02-18 16:18:43 -0500

    spacing

--- lab1/src/lab1/Main.java
+++ lab1/src/lab1/Main.java
@@ -4,25 +4,30 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String args[]) throws IOException {
+
 		Scanner in = new Scanner(System.in);
 		int yearsToWork = in.nextInt();
-
 		double workingReturn; 		
+		double retiredReturn;
+
 		do {
 			System.out.println("Please enter your expected working return as an integer between 0 and 20: \n"
 					+ "e.g. 7 ");
 			workingReturn = in.nextDouble();
-			} while(workingReturn < 0 || workingReturn > 20);
+		}
+		while(workingReturn < 0 || workingReturn > 20);
 		workingReturn /= 100;
-		double retiredReturn;
+
 		do {
 			System.out.println("Please enter your expected retired return as an integer between 0 and 3: \n"
 					+ "e.g. 2");
 			retiredReturn = in.nextDouble();
-		} while(retiredReturn< 0 || retiredReturn > 3);
+		} 
+		while(retiredReturn< 0 || retiredReturn > 3);
 		retiredReturn /= 100;
-		
+
 		int requiredIncome = in.nextInt();
 		double monthlySSI = in.nextDouble();
 	}
