76cebe17f62b0056f55e778688549b4955a8ef23
Robert Weber <robertweber95@gmail.com>
2015-02-19 19:25:30 -0500

    Added calculatePV method and finished input prompts

--- lab1/src/lab1/Main.java
+++ lab1/src/lab1/Main.java
@@ -1,6 +1,5 @@
 package lab1;
 
-import java.io.IOException; 
 import java.util.Scanner;
 
 public class Main {
@@ -8,12 +7,18 @@ public class Main {
 	public static void main(String args[]){
 
 		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please enter the number of years you will be working: \n");
 		int yearsToWork = in.nextInt();
+		
+		System.out.println("Please enter your expected number of years in retirement: \n");
+		int yearsRetired = in.nextInt();
+		
 		double workingReturn; 		
 		double retiredReturn;
 
 		do {
-			System.out.println("Please enter your expected working return as an integer between 0 and 20: \n"
+			System.out.println("Please enter your expected working annual return as an integer between 0 and 20: \n"
 					+ "e.g. 7 ");
 			workingReturn = in.nextDouble();
 		}
@@ -21,15 +26,33 @@ public class Main {
 		workingReturn /= 100;
 
 		do {
-			System.out.println("Please enter your expected retired return as an integer between 0 and 3: \n"
+			System.out.println("Please enter your expected retired annual return as an integer between 0 and 3: \n"
 					+ "e.g. 2");
 			retiredReturn = in.nextDouble();
 		} 
 		while(retiredReturn< 0 || retiredReturn > 3);
 		retiredReturn /= 100;
 
-		int requiredIncome = in.nextInt();
+		System.out.println("Please enter your required income during retirement as a dollar amount: \n");
+		double requiredIncome = in.nextDouble();
+		
+		System.out.println("Please enter your monthly Social Security income during retirement as a dollar amount: \n");
 		double monthlySSI = in.nextDouble();
+		
+		System.out.printf("The amount you need to save for retirement is $%.2f", 
+				calculatePV(retiredReturn, yearsRetired, monthlySSI, requiredIncome));
+		
+		in.close();	
+		
 	}
+	
+	 private static double calculatePV (double retiredReturn, int yearsRetired, 
+				double monthlySSI, double requiredIncome) {
+			
+			double cashFlow = requiredIncome-monthlySSI;
+			
+			return cashFlow*(1-(1+ Math.pow(1+(retiredReturn/12), -(yearsRetired)*12)))/(retiredReturn/12);
+		
+		}
 
 }
--- lab1/src/lab1/Robert.java
+++ lab1/src/lab1/Robert.java
@@ -5,11 +5,16 @@ public class Robert {
 	 double calculatePV (double retiredReturn, int yearsRetired, 
 			double monthlySSI, double requiredIncome) {
 		
-		double pv = 0.0;
+		double cashFlow = requiredIncome-monthlySSI;
 		
-		
-		return pv;
+		return cashFlow*(1-(1+ Math.pow(1+(retiredReturn/12), -(yearsRetired)*12)))/(retiredReturn/12);
 	
 	}
+	 
+	 double calculatePMT(){
+		 
+		 double pmt = 0.0;
+		 return pmt;
+	 }
 
 }
